f27e9db6519a947ab480e4667199d46e47a0ae9e
ameer <ameer@V>
2024-11-22 12:02:43 +0100

    Add project files.

---
+++ Lab 1 (grupp 70)/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Lab_1__grupp_70_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int menuSelection = -1;
+            int hoursStockholm = 14;
+            int minutesStockholm = 3;
+            int hoursNewyork = 10;
+            int minutesNewyork = 10;
+            int timeDifference = 6;
+            int travelHours = 7;
+            int travelMinutes = 25;
+            while (menuSelection != 3)
+            {
+                //The clearing method is used to clear the console before the loop starts again.
+
+                Console.Clear();
+                Console.WriteLine("Hello! , Welcome to the flight schedule calculator!\n" + 
+                                  "Which flight would you like to know more about?\n" +
+                                  "1. Stockholm - New York\n" +
+                                  "2. New York - Stockholm\n" +
+                                  "3. Exit program");
+
+                //We put a \n > to make it more clear that a input is required to advance
+
+                Console.Write("\n> ");
+                menuSelection = Int32.Parse(Console.ReadLine());
+
+
+
+
+            }
+            Console.WriteLine("The program will exit now!");
+        }
+    }
+}
